31e960a5576fe4b2f9f5b1c71a2c92ca2b3e1c60
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-18 20:03:22 +0600

    login page complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
 
     </div>
---
+++ src/pages/Auth/Login.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Container, Row, Col, FloatingLabel, Form } from 'react-bootstrap'
+import { RiLoginCircleLine } from 'react-icons/ri'
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import SocialLogin from './SocialLogin';
+import './auth.css'
+
+const Login = () => {
+
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    const handleLogin = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className='auth'>
+            <Container>
+                <Row className='justify-content-center align-items-center h-100vh'>
+                    <Col md={6} lg={5}>
+
+                        <div className="card" >
+
+                            <div className="d-flex justify-content-center mb-4">
+                                <div><h2 className="form__title ">Login</h2></div>
+                            </div>
+
+
+                            <form onSubmit={handleSubmit(handleLogin)}>
+
+                                <div>
+                                    <Form.Label htmlFor="email" className='ps-1'>Email</Form.Label>
+                                    <Form.Control type="email" {...register('email')} placeholder='Your Email' />
+                                </div>
+
+                                <div className='mt-4'>
+                                    <Form.Label htmlFor="passowrd" className='ps-1'>Passowrd</Form.Label>
+                                    <Form.Control type="passowrd" {...register('passowrd')} placeholder='Your Passowrd' />
+                                </div>
+                        
+
+                                <div className="form__terms d-flex justify-content-center">
+                                    <Form.Group className="my-3" id="formGridCheckbox">
+                                        <Form.Check inline type="checkbox" id='remember' label="Remember Me" />
+                                    </Form.Group>
+                                </div>
+
+
+                                <button className='btn form__btn' type="submit">
+                                    Login
+                                    <RiLoginCircleLine className='form__btn-icon' />
+                                </button>
+                            </form>
+
+                            <div className="form__detail mt-3 d-flex justify-content-center">
+                                <div>
+                                    <p>If you haven't account <Link to="/register">Create account here</Link></p>
+                                    <p className='text-center'>Forgot Password <Link to="/reset">Reset here</Link></p>
+                                </div>
+                            </div>
+
+                            <div className="form__or d-flex">
+                                <div className='w-100 form__or-hr'><hr /></div>
+                                <div className='mx-2 mt-1 or'>OR</div>
+                                <div className='w-100 form__or-hr'><hr /></div>
+                            </div>
+
+                            {/* social login components */}
+                            <SocialLogin></SocialLogin>
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Login;
